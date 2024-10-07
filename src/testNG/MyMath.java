e62e0c1ee325c660dc74023d337bb6aaa4bc200c
Casua <Casua@Mert>
2024-10-07 17:35:28 +0200

    Adding new project

---
+++ src/testNG/MyMath.java
@@ -0,0 +1,16 @@
+package testNG;
+
+public class MyMath {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
